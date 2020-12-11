79a7a6d08e32dcc5a901f4d8dcdc9ac0b27d2294
Suhas Kadu <kadusuhas72@gmail.com>
2020-12-11 17:48:22 +0530

    feat:colors replaced with images

--- MyColorViews2/app/src/main/java/com/example/mycolorviews/MainActivity.kt
+++ MyColorViews2/app/src/main/java/com/example/mycolorviews/MainActivity.kt
@@ -18,11 +18,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view: View){
         when(view.id){
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.img1)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.img2)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.img3)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.img4)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.img5)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
