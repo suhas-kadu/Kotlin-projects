f166d999d24472023ce53e72c3700f4cf4cbc0b6
Suhas Kadu <kadusuhas72@gmail.com>
2020-12-16 19:06:01 +0530

    feat:fragments and navigation

--- AboutMe2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AboutMe2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,52 +9,81 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var doneButton:Button
+    lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
-        doneButton = findViewById<Button>(R.id.done_btn)
-                doneButton.setOnClickListener {
-            addNickName(it)
-        }
-
-        findViewById<TextView>(R.id.nickName).setOnClickListener {
-            updateNickName(it)
-        }
+          binding.doneBtn.setOnClickListener{
+              addNickName(it)
+          }
+//        doneButton = findViewById<Button>(R.id.done_btn)
+//                doneButton.setOnClickListener {
+//            addNickName(it)
+//        }
+//
+//        findViewById<TextView>(R.id.nickName).setOnClickListener {
+//            updateNickName(it)
+//        }
     }
 
-
     private fun addNickName(view: View) {
-        val editText=findViewById<EditText>(R.id.editText)
-        val outputText=findViewById<TextView>(R.id.nickName)
 
-        outputText.text = editText.text
+//        myName?.nickname = binding.nickName.text.toString()
+//        binding.editText.visibility = View.GONE
+//        binding.doneBtn.visibility = View.GONE
+//        binding.nickName.visibility = View.VISIBLE
 
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        outputText.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nickName.text.toString()
+            nickName.visibility = View.VISIBLE
+
+            invalidateAll()
+            editText.visibility = View.GONE
+            doneBtn.visibility = View.GONE
+            nickName.visibility = View.VISIBLE
+        }
 
         // Hiding the keyboard
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    private fun updateNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
+//    private fun addNickName(view: View) {
+//        val editText=findViewById<EditText>(R.id.editText)
+//        val outputText=findViewById<TextView>(R.id.nickName)
+//
+//        outputText.text = editText.text
+//
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        outputText.visibility = View.VISIBLE
+//
+//        // Hiding the keyboard
+//        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+//    }
 
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
-
-        editText.requestFocus()
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText,0)
-
-    }
+//    private fun updateNickName(view: View) {
+//        val editText = findViewById<EditText>(R.id.editText)
+//
+//        editText.visibility = View.VISIBLE
+//        doneButton.visibility = View.VISIBLE
+//        view.visibility = View.GONE
+//
+//        editText.requestFocus()
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.showSoftInput(editText,0)
+//
+//    }
 
 }
---
+++ AboutMe2/app/src/main/java/com/example/myapplication/MyName.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+data class MyName(var name: String = "", var nickname: String = "") {
+
+}
